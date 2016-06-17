bcd8e031920f66098112c7ea715a772c8e1ff881
bala <sbm.kvp@gmail.com>
2016-06-17 22:05:10 +0100

    initial login change

--- scripts/login.php
+++ scripts/login.php
@@ -2,11 +2,11 @@
 	if ( isset($_POST['pass']) ) {
 		session_start();	
 		$pass = $_POST['pass'];
-		if($pass==file_get_contents("../data/up")) { 
-			$_SESSION['login']='user';
-			echo 'success';
-		} else if ($pass==file_get_contents("../data/ap")) {
+		if($pass==file_get_contents("../data/ap")) { 
 			$_SESSION['login']='admin';
+			echo 'success';
+		} else if ($pass==file_get_contents("../data/up")) {
+			$_SESSION['login']='user';
 			echo 'success';
 		}
 	} else {
